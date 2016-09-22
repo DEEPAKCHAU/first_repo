711c1d1809e82322106c02a21d1e1afed3d7b456
unknown <siddharthdu@siddharthdu.cybage.com>
2016-09-22 17:13:36 +0530

    first commit practice

---
+++ src/tester.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author siddharthdu
+ *
+ */
+public class tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Deepak to GIT");
+
+	}
+
+}
